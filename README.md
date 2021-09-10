1d39da401a030b62b44ace2e0d6de3cdcd6c3390
danielturcios <danielturciosa@gmail.com>
2021-09-09 20:34:55 -0700

    added json db support

--- README.md
+++ README.md
@@ -39,6 +39,9 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
+### `npm run server`
+Starts a local json server on port 3001. Imitates a database using the data found in db.json. Can access the database via http://localhost:3001/persons
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import Filter from './components/Filter'
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import "./App.css"
 
 function App() {
-  const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setName ] = useState('')
   const [ newNumber, setNumber ] = useState('')
   const [ newFilter, setFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
 
   let filteredPersons = persons
   if (newFilter) {
